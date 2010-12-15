01e9d96bcf641c527ac426c8769d66af1bef8da9
Ronan Rodrigo Nunes <ronanchiwi@gmail.com>
2010-12-15 05:17:49 -0200

    alterado routes para se adaptar ao login. adicionado novos objetos no imprimir.css e no sicoca.css

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Sicoca::Application.routes.draw do
+
+  resources :sessions
   resources :placas
   resources :tipo_usuarios
   resources :pessoas
+  resources :home
 
   resources :cabo_eleitorals , :belongs_to => :pessoa
   resources :carro_soms , :belongs_to => :pessoa
   resources :doacaos , :belongs_to => :pessoa
   resources :usuarios , :belongs_to => :pessoa
-  resources :usuarios , :belongs_to =>  :tipo_usuariosª
+  resources :usuarios , :belongs_to => :tipo_usuarios
   resources :cavaletes, :belongs_to => :placas
   resources :placa_casas, :belongs_to => :pessoa
   resources :placa_casas, :belongs_to => :placas
 
-  root :to => "home#index"
+  root :to => "sessions#new"
 end
